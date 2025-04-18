67dcbea2939ec18ed92680c2c956b83c314cc25d
Obaydullah <obaydullahkhaan@gmail.com>
2025-04-18 23:35:37 +0600

    bd access added

--- database/mysql.go
+++ database/mysql.go
@@ -5,5 +5,12 @@ import (
 )
 
 func SaveUrl(shortKey, originalUrl string) error {
-	_, err := config.DB.Exec
+	_, err := config.DB.Exec("INSERT INTO urls(shortKey, originalUrl) VALUES(?, ?)", shortKey, originalUrl)
+	return err
+}
+
+func GetUrl(shortKey string) (string, error) {
+	var url string
+	err := config.DB.QueryRow("SELECT url FROM urls WHERE shortKey=?", shortKey).Scan(&url)
+	return url, err
 }
